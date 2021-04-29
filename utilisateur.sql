01cf612a8b9edc954f32eb1cf97ea0a3c662f90d
Anne Wilga <anne.wilga@orange.fr>
2021-04-29 17:17:38 +0200

    creation d'utilisateur pour anne,julien,vincent,antoine,marion,fode

---
+++ utilisateur.sql
@@ -0,0 +1,9 @@
+CREATE USER IF NOT EXISTS 'anne'@'localhost' identified by 'ANN1635u!';
+CREATE USER IF NOT EXISTS 'julien'@'localhost' identified by 'JUL1636u!';
+CREATE USER IF NOT EXISTS 'vincent'@'localhost' identified by 'VIN1637u!';
+CREATE USER IF NOT EXISTS 'antoine'@'localhost' identified by 'ANT1638u!';
+CREATE USER IF NOT EXISTS 'marion'@'localhost' identified by 'MAR1639u!';
+CREATE USER IF NOT EXISTS 'fode'@'localhost' identified by 'FOD1640u!';
+CREATE USER IF NOT EXISTS 'utilisateur'@'localhost' identified by 'UTI1641u!';
+CREATE USER IF NOT EXISTS 'internaute'@'localhost' identified by 'INT1642u!';
+
